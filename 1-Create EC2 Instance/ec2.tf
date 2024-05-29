f3ed70220b56fca67844193cf965087fe3bd3716
Racheltembi <114146038+Racheltembi@users.noreply.github.com>
2024-05-29 23:31:11 +0100

    Update ec2.tf

--- 1-Create EC2 Instance/ec2.tf
+++ 1-Create EC2 Instance/ec2.tf
@@ -1,16 +1,16 @@
 # Below is the resource block which creates EC2 Instance
 resource "aws_instance" "prod-instance" {
-  ami           = ""
-  instance_type = ""
+  ami           = "ami-0d3a2960fcac852bc"
+  instance_type = "t3.micro"
   tags = {
-    Name = ""
+    Name = "prod-instance"
   }
 }
 
 
 # Below is the provider which helps in connecting with AWS Account
 provider "aws" {
-  region = ""
+  region     = "eu-north-1"
   access_key = ""
   secret_key = ""
 }
