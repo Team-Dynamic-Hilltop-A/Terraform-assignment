fc5db1b75e65d21a0240013f3e75f9cde1889df2
Tatapong <info.salesvaccine@gmail.com>
2024-05-30 12:25:42 +0100

    i have created a vpc, an ec2 instance, an igw, a two subnets, route table and have associated the route table. the codes for these different infrastructure were taken from gpt

---
+++ 1-Create EC2 Instance/associate-route-table.tf
@@ -0,0 +1,5 @@
+# Associate public subnet with public route table
+resource "aws_route_table_association" "public_subnet_route_table_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
---
+++ 1-Create EC2 Instance/private-subnet.tf
@@ -0,0 +1,11 @@
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "Private Subnet"
+  }
+}
+
